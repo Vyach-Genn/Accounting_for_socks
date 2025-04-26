dad2929bd5250bca7f812f96362770a0e4ebaf04
vachg <vachgen@yandex.ru>
2025-04-26 15:11:35 +0300

    Start

---
+++ src/main/java/pro_sky/accounting_for_socks/AccountingForSocksApplication.java
@@ -0,0 +1,13 @@
+package pro_sky.accounting_for_socks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingForSocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingForSocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro_sky/accounting_for_socks/AccountingForSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package pro_sky.accounting_for_socks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingForSocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
